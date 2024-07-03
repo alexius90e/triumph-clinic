0c301de7f6841c960efc354da3a4019dcf3c1937
Aliaksei Karzhou <alexius90e@gmail.com>
2024-07-03 16:42:09 +0300

    feat: added nav adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,4 @@ import './sliders.js';
 import './step-by-step.js';
 import './team.js';
 import './faq.js';
+import './nav.js';
---
+++ assets/js/nav.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__nav');
+
+if (burger && headerNav) {
+  burger.addEventListener('click', () => {
+    headerNav.classList.toggle('active');
+    burger.classList.toggle('active');
+  });
+
+  headerNav.addEventListener('click', (event) => {
+    const isLink = event.target.classList.contains('header__nav-link');
+    const isPhone = event.target.classList.contains('header__nav-phone');
+    const isButton = event.target.classList.contains('button');
+
+    if (isLink || isPhone || isButton) {
+      headerNav.classList.remove('active');
+      burger.classList.remove('active');
+    }
+  });
+}
