1598c9c379a8bfe491c99b21321c4aa3f7249151
Aliaksei Karzhou <alexius90e@gmail.com>
2024-07-05 15:38:37 +0300

    feat: added ability to send form data to tg

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -22,15 +22,31 @@ callbackButtons.forEach((button) => {
 const modalForms = document.querySelectorAll('.modal__form');
 const callbackForms = document.querySelectorAll('.callback__form');
 
+const BOT_TOKEN = '';
+const CHAT_ID = '';
+
 [...modalForms, ...callbackForms].forEach((form) => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const name = formData.get('name');
     const phone = formData.get('phone');
-    console.log(name, phone)
+    const message = `Имя: ${name} Телефон ${phone}`;
+    sendMessage(message);
     event.currentTarget.reset();
     if (callbackModal) callbackModal.classList.remove('active');
     if (thanksModal) thanksModal.classList.add('active');
   });
 });
+
+function sendMessage(message) {
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=html&text=${message}`;
+
+  fetch(url)
+    .then((data) => {
+      console.log('Message sent successfully:', data);
+    })
+    .catch((error) => {
+      console.error('Error sending message:', error);
+    });
+}
