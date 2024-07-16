2f3396d8b364f17378aeed088fe6755f961f54f8
Aliaksei Karzhou <alexius90e@gmail.com>
2024-07-16 23:38:53 +0300

    feat: added dinamic addition of videos to dom

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -7,8 +7,21 @@ reviewsSlideVideos.forEach((videoEl) => {
     reviewsSlideVideos.forEach((video) => {
       if (video === event.currentTarget) {
         video.classList.add('active');
+        video.innerHTML = `
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/XIMLoLxmTDw?si=wFzm5etjCkaNdMl5"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        `;
       } else {
         video.classList.remove('active');
+        video.innerHTML = '';
       }
     });
   });
